db4a37705a3153e9a07c04a5f93f499501a2acf4
Karri Moilanen <kmoilane@c3r5p6.hive.fi>
2020-01-15 06:47:03 +0200

    portsentry

---
+++ roger_skyline_1/scripts/portsentry_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo sed -i 's/TCP_MODE="tcp"/TCP_MODE="atcp"/g' /etc/defaults/portsentry
+sudo sed -i 's/UDP_MODE="udp"/UDP_MODE="audp"/g' /etc/defaults/portsentry
+sudo sed -i 's/BLOCK_UDP="0"/BLOCK_UDP="1"/g' /etc/portsentry/portsentry.conf
+sudo sed -i 's/BLOCK_TCP="0"/BLOCK_TCP="1"/g' /etc/portsentry/portsentry.conf
+sudo sed -i 's/KILL_ROUTE="\/sbin\/route add -host $TARGET$ reject"/#KILL_ROUTE="\/sbin\/route add -host $TARGET$ reject"/g' /etc/portsentry/portsentry.conf
+sudo sed -i 's/#KILL_ROUTE="\/sbin\/iptables -I INPUT -s $TARGET$ -j DROP"/KILL_ROUTE="\/sbin\/iptables -I INPUT -s $TARGET$ -j DROP"/g' /etc/portsentry/portsentry.conf
+sudo systemctl restart portsentry
