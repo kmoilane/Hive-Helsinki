beca4ac6db2d97aac8b8bf28d6a42dbb1a899634
Karri Moilanen <kmoilane@c3r5p6.hive.fi>
2020-01-15 12:19:16 +0200

    ssl script

---
+++ roger_skyline_1/scripts/ssl_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt
+
+echo 'SSLCipherSuite EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH
+SSLProtocol All -SSLv2 -SSLv3 -TLSv1 -TLSv1.1
+SSLHonorCipherOrder On
+# Disable preloading HSTS for now.  You can use the commented out header line that includes
+# the "preload" directive if you understand the implications.
+# Header always set Strict-Transport-Security "max-age=63072000; includeSubDomains; preload"
+Header always set X-Frame-Options DENY
+Header always set X-Content-Type-Options nosniff
+# Requires Apache >= 2.4
+SSLCompression off
+SSLUseStapling on
+SSLStaplingCache "shmcb:logs/stapling-cache(150000)"
+# Requires Apache >= 2.4.11
+SSLSessionTickets Off' | sudo tee /etc/apache2/conf-available/ssl-params.conf
+
+sudo cp /etc/apache2/sites-available/default-ssl.conf /etc/apache2/sites-available/default-ssl.conf.bak
+sudo sed -i 's/ServerAdmin webmaster@localhost/ServerAdmin kmoilane@student.hive.fi/g' /etc/apache2/sites-available/default-ssl.conf
+sudo sed -i	'4i\ServerName 10.13.13.13' /etc/apache2/sites-available/default-ssl.conf
+sudo sed -i 's/SSLCertificateFile      \/etc\/ssl\/certs\/ssl-cert-snakeoil.pem/SSLCertificateFile      \/etc\/ssl\/certs\/apache-selfsigned.crt/g' /etc/apache2/sites-available/default-ssl.conf
+sudo sed -i 's/SSLCertificateKeyFile \/etc\/ssl\/private\/ssl-cert-snakeoil.key/SSLCertificateKeyFile \/etc\/ssl\/private\/apache-selfsigned.key/g' /etc/apache2/sites-available/default-ssl.conf
+
