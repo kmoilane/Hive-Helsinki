06e4487ec934655bb6ffa362e31406ad8ef5cb2c
Karri Moilanen <kmoilane@c3r5p6.hive.fi>
2020-01-15 06:00:22 +0200

    dos_script

---
+++ roger_skyline_1/scripts/dos_script.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo '[sshd]
+enabled = true
+port    = 55155
+filter  = sshd
+logpath = %(sshd_log)s
+maxretry = 5
+bantime = 300
+banaction = iptables-multiport
+
+[http-get-dos]
+enabled = true
+port    = http,https
+filter  = http-get-dos
+logpath = /var/log/apache2/access.log
+maxretry = 50
+findtime = 120
+bantime =  60
+action  = iptables[name=HTTP, port=http, protocol=tcp]' | sudo tee /etc/fail2ban/jail.d/defaults-debian.local >> /dev/null
+
+echo '[Definition]
+failregex = ^<HOST> -.*"(GET|POST).*
+ignoreregex =' | sudo tee /etc/fail2ban/filter.d/http-get-dos.conf >> /dev/null
+
+sudo systemctl restart fail2ban
+sudo systemctl status fail2ban
