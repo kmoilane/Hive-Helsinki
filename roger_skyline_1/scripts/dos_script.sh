5c83e6c7c5732fcc6cb0a2d00e7e87ea9b9c1a59
Karri Moilanen <kmoilane@c3r5p6.hive.fi>
2020-01-15 15:27:10 +0200

    done

--- roger_skyline_1/scripts/dos_script.sh
+++ roger_skyline_1/scripts/dos_script.sh
@@ -9,6 +9,13 @@ maxretry = 5
 bantime = 300
 banaction = iptables-multiport
 
+[portscan]
+enabled	= true
+filter	= portscan
+logpath	= /var/log/syslog
+maxretry = 1
+bantime	= 300
+
 [http-get-dos]
 enabled = true
 port    = http,https
@@ -22,6 +29,10 @@ action  = iptables[name=HTTP, port=http, protocol=tcp]' | sudo tee /etc/fail2ban
 echo '[Definition]
 failregex = ^<HOST> -.*"(GET|POST).*
 ignoreregex =' | sudo tee /etc/fail2ban/filter.d/http-get-dos.conf >> /dev/null
+
+echo '[Definition]
+failregex = UFW BLOCK.* SRC=<HOST>
+ignoreregex =' | sudo tee /etc/fail2ban/filter.d/portscan.conf >> /dev/null
 
 sudo systemctl restart fail2ban
 sudo fail2ban-client status
