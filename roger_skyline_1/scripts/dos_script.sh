537dc904308147721f024971d80936f1e4dfb8a4
Karri Moilanen <kmoilane@c3r5p6.hive.fi>
2020-01-16 14:32:03 +0200

    fixed port

--- roger_skyline_1/scripts/dos_script.sh
+++ roger_skyline_1/scripts/dos_script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo '[sshd]
+echo "[sshd]
 enabled = true
-port    = 55155
+port    = $1
 filter  = sshd
 logpath = %(sshd_log)s
 maxretry = 5
@@ -15,6 +15,7 @@ filter	= portscan
 logpath	= /var/log/syslog
 maxretry = 1
 bantime	= 300
+findtime = 300
 
 [http-get-dos]
 enabled = true
@@ -24,15 +25,15 @@ logpath = /var/log/apache2/access.log
 maxretry = 50
 findtime = 120
 bantime =  60
-action  = iptables[name=HTTP, port=http, protocol=tcp]' | sudo tee /etc/fail2ban/jail.d/defaults-debian.local >> /dev/null
+action  = iptables[name=HTTP, port=http, protocol=tcp]" | sudo tee /etc/fail2ban/jail.d/defaults-debian.local >> /dev/null
 
-echo '[Definition]
-failregex = ^<HOST> -.*"(GET|POST).*
-ignoreregex =' | sudo tee /etc/fail2ban/filter.d/http-get-dos.conf >> /dev/null
+echo "[Definition]
+failregex = ^<HOST> -.*\"(GET|POST).*
+ignoreregex =" | sudo tee /etc/fail2ban/filter.d/http-get-dos.conf >> /dev/null
 
-echo '[Definition]
+echo "[Definition]
 failregex = UFW BLOCK.* SRC=<HOST>
-ignoreregex =' | sudo tee /etc/fail2ban/filter.d/portscan.conf >> /dev/null
+ignoreregex =" | sudo tee /etc/fail2ban/filter.d/portscan.conf >> /dev/null
 
 sudo systemctl restart fail2ban
 sudo fail2ban-client status
