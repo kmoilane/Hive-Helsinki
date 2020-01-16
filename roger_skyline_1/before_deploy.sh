6b47fac0fe4ea5188d687c45335077505e29a04a
Karri Moilanen <kmoilane@c1r1p8.hive.fi>
2020-01-16 12:44:34 +0200

    changed the dev/null

--- roger_skyline_1/before_deploy.sh
+++ roger_skyline_1/before_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt-get install -y sudo vim ssh apache2 fail2ban ufw portsentry mailutils >> /dev/null
+apt-get install -y sudo vim ssh apache2 fail2ban ufw portsentry mailutils
 
 echo "
 Type your non root username for sudo rights:"
